23b1e9c555ff782c508e0c31de8462b4172bbbde
Ivan Slobodian <campersf@gmail.com>
2022-06-22 15:06:42 +0300

    minor fixes

--- react_signUp/src/components/SignUp/SignUp.js
+++ react_signUp/src/components/SignUp/SignUp.js
@@ -1,39 +1,41 @@
 import React from 'react';
-import classes from './SignUp.module.css';
+import classes from './SignUp.module.scss';
 
-import initialState from './utils/initialState';
+import fields from './utils/initialState';
 
 class SignUp extends React.Component {
   constructor() {
     super();
-    const { fields } = initialState;
     this.state = { fields };
   }
 
   handleValueChange = (event) => {
     const { value, name } = event.target;
-    const { validator } = this.state.fields[name];
+    const { fields } = this.state;
+    const { validator } = fields[name];
 
-    const password = undefined || this.state.fields.password.value;
+    const password = fields.password.value;
     const error = validator(value, password);
 
     this.setState({
       fields: {
-        ...this.state.fields,
-        [name]: { ...this.state.fields[name], value: value, error },
+        ...fields,
+        [name]: { ...fields[name], value: value, error },
       },
     });
   };
 
-  handleErrors = () => {
-    const isError = Object.values(this.state.fields)
+  getFormError = () => {
+    const { fields } = this.state;
+
+    const isError = Object.values(fields)
       .map((field) => field.error)
       .some((error) => error);
+
     return isError;
   };
 
   handleReset = () => {
-    const { fields } = initialState;
     this.setState({ fields });
   };
 
@@ -44,28 +46,28 @@ class SignUp extends React.Component {
 
   render() {
     const { fields } = this.state;
-
     return (
       <>
         <form onSubmit={this.handleSubmit} className={classes['sign-form']}>
-          {Object.entries(fields).map((field, index) => {
+          {Object.entries(fields).map(([fieldName, fieldValue]) => {
+            const { title, type, name, value, placeholder, error } = fieldValue;
             return (
-              <label name={field[0]} key={index}>
+              <label name={fieldName} key={fieldName}>
                 <input
-                  title={field[1].title}
-                  type={field[1].type}
-                  name={field[1].name}
-                  value={field[1].value}
-                  placeholder={field[1].placeholder}
+                  title={title}
+                  type={type}
+                  name={name}
+                  value={value}
+                  placeholder={placeholder}
                   onChange={this.handleValueChange}
                 />
-                <div className={classes.error}>{field[1].error}</div>
+                <div className={classes.error}>{error}</div>
               </label>
             );
           })}
           <div className={classes.container}>
             <button onClick={this.handleReset}>Reset</button>
-            <button type="submit" disabled={this.handleErrors()}>
+            <button type="submit" disabled={this.getFormError()}>
               Submit
             </button>
           </div>
--- react_signUp/src/components/SignUp/utils/initialState.js
+++ react_signUp/src/components/SignUp/utils/initialState.js
@@ -1,57 +1,54 @@
 export default {
-  fields: {
-    name: {
-      title: 'Name',
-      type: 'name',
-      name: 'name',
-      value: '',
-      error: true,
-      placeholder: 'Input your name..',
-      validator: (value = '') => {
-        return value.length >= 2 ? false : 'Name is too short';
-      },
-    },
-    email: {
-      title: 'Email',
-      type: 'email',
-      name: 'email',
-      value: '',
-      error: true,
-      placeholder: 'Input your email..',
-      validator: (value = '') => {
-        return value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
-          ? false
-          : 'Email is invalid';
-      },
-    },
-    password: {
-      title: 'Password',
-      type: 'password',
-      name: 'password',
-      autoComplete: 'false',
-      value: '',
-      error: true,
-      placeholder: 'Input your password..',
-      validator: (value = '') => {
-        return value.match(
-          /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g
-        )
-          ? false
-          : 'Password is too simple';
-      },
-    },
-    passwordConfirm: {
-      title: 'Password confirm',
-      type: 'password',
-      name: 'passwordConfirm',
-      autoComplete: 'false',
-      value: '',
-      error: true,
-      placeholder: 'Confirm your password..',
-      validator: (value = '', allValues) => {
-        return value === allValues ? false : 'Passwords dont match';
-      },
+  name: {
+    title: 'Name',
+    type: 'name',
+    name: 'name',
+    value: '',
+    error: true,
+    placeholder: 'Input your name..',
+    validator: (value = '') => {
+      return value.length >= 2 ? false : 'Name is too short';
     },
   },
-  isError: true,
+  email: {
+    title: 'Email',
+    type: 'email',
+    name: 'email',
+    value: '',
+    error: true,
+    placeholder: 'Input your email..',
+    validator: (value = '') => {
+      return value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
+        ? false
+        : 'Email is invalid';
+    },
+  },
+  password: {
+    title: 'Password',
+    type: 'password',
+    name: 'password',
+    autoComplete: 'false',
+    value: '',
+    error: true,
+    placeholder: 'Input your password..',
+    validator: (value = '') => {
+      return value.match(
+        /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g
+      )
+        ? false
+        : 'Password is too simple';
+    },
+  },
+  passwordConfirm: {
+    title: 'Password confirm',
+    type: 'password',
+    name: 'passwordConfirm',
+    autoComplete: 'false',
+    value: '',
+    error: true,
+    placeholder: 'Confirm your password..',
+    validator: (value = '', allValues) => {
+      return value === allValues ? false : 'Passwords dont match';
+    },
+  },
 };
